3fdf3d5aa4e8b218c297c4512b0bd54bb32dafbd
Socerer <coolfool@socerer>
2022-03-15 01:35:35 +0530

    Setup basic layout

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
-const express=require('express')
+const express = require("express");
 // const app=express()
+const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-const router=express.Router();
-
-router.get('/', (req,res)=>{
-    res.send('hello')
-})
-
-module.exports=router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,40 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-require("dotenv").config();
+// require("dotenv").config();
 
 // Import Routes
 const indexRouter = require("./routes/index");
 // const homeRouter=require('/')
 
-// Database Connection
-mongoose.connect(process.env.MONGO_URI),()=>{console.log('Connected to Database!')};
+// Middlewares
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("layout", "layouts/layout");
+// app.set("views", __dirname + "views/");
+// app.set("views", "views/");
+app.use(expressLayouts);
+app.use(express.static(__dirname + "public"));
 
+// Database Connection
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}),
+  
+console.log("Connected!!!");
 const db = mongoose.connection;
 
-db.on("error", error => {
+db.on("error", (error) => {
   console.error(error);
 });
 
 db.once("open", () => {
   console.log("Database connected!");
 });
-
-// Middlewares
-app.set("view-engine", "ejs");
-app.set("views", __dirname + "/views");
-app.set("layouts", "layouts/layout");
-app.use(expressLayouts);
-app.use(express.static("public"));
 
 // Route Handlers
 app.use("/", indexRouter);
